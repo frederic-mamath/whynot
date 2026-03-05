dd0bdfee5d7e9fa4a72db0d185b57c035effb0a5
Frederic MAMATH <fredericmamath@gmail.com>
2026-03-05 22:34:50 +0100

    feat: adds style for title

--- app/client/src/pages/WelcomePage/WelcomePage.tsx
+++ app/client/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,9 +1,21 @@
 import ButtonV2 from "@/components/ui/ButtonV2/ButtonV2";
 
+const primaryColor = "rgb(224, 255, 0)";
+
 const WelcomePage = () => {
   return (
     <div>
-      <div>Popup</div>
+      <div
+        style={{
+          color: primaryColor,
+          fontSize: "42px",
+          fontFamily: "Outfit, sans-serif",
+          fontWeight: 900,
+          letterSpacing: "-1.5px",
+        }}
+      >
+        popup
+      </div>
       <div>Achète et vends en live.</div>
       <div>Rejoins la communauté de live shopping.</div>
       <div>Regarde, achète, vends - en quelques taps.</div>
