d13c9ce77a7d80f0d358d0037f1cf0bdc674cda7
Frederic MAMATH <fredericmamath@gmail.com>
2026-03-06 18:22:57 +0100

    fix: navigation

--- app/client/src/pages/WelcomePage/WelcomePage.tsx
+++ app/client/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,7 +1,8 @@
-import AppleIcon from "@/components/Icons/AppleIcon/AppleIcon";
-import GoogleIcon from "@/components/Icons/GoogleIcon";
 import MailIcon from "@/components/Icons/MailIcon";
 import ButtonV2 from "@/components/ui/ButtonV2/ButtonV2";
+import OAuthButtons from "@/components/OAuthButtons";
+import OrDivider from "@/components/OrDivider/OrDivider";
+import { Link } from "react-router-dom";
 
 const WelcomePage = () => {
   return (
@@ -21,25 +22,8 @@ const WelcomePage = () => {
       </div>
 
       <div className="flex flex-col items-center justify-center gap-[10px] w-full max-w-[327px]">
-        <ButtonV2
-          href="/auth/apple"
-          label="Continuer avec Apple"
-          className="bg-white text-black w-full font-bold"
-          icon={<AppleIcon />}
-        />
-        <ButtonV2
-          href="/auth/google"
-          label="Continuer avec Google"
-          className="text-white w-full border border-border bg-[rgb(26,_26,_26)] font-bold"
-          icon={<GoogleIcon />}
-        />
-
-        <div className="flex items-center gap-3 w-full py-1">
-          <div className="flex-1 h-px bg-border" />
-          <div className="text-border text-sm">OU</div>
-          <div className="flex-1 h-px bg-border" />
-        </div>
-
+        <OAuthButtons />
+        <OrDivider />
         <ButtonV2
           label="S'inscrire avec un email"
           className="text-primary w-full border border-primary bg-transparent font-bold"
@@ -49,9 +33,9 @@ const WelcomePage = () => {
 
         <div className="font-outfit text-muted-foreground text-[13px] leading-[18px] flex gap-1">
           <div>Déjà un compte ?</div>
-          <a href="/login" className="text-primary">
+          <Link to="/login" className="text-primary">
             Se connecter
-          </a>
+          </Link>
         </div>
       </div>
     </div>
