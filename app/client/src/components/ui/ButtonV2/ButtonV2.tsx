192d9a6345105a4c41f4c20379f29b4c8f4657f5
Frederic MAMATH <fredericmamath@gmail.com>
2026-03-06 16:31:46 +0100

    fix: hovering style

--- app/client/src/components/ui/ButtonV2/ButtonV2.tsx
+++ app/client/src/components/ui/ButtonV2/ButtonV2.tsx
@@ -8,10 +8,9 @@ interface Props {
 const ButtonV2 = ({ icon, label, href, style }: Props) => {
   return (
     <button
+      className="relative overflow-hidden transition-colors before:pointer-events-none before:absolute before:inset-0 before:bg-white/10 before:opacity-0 before:transition-opacity hover:before:opacity-100 hover:cursor-pointer"
       style={{
         display: "flex",
-        paddingTop: "14px",
-        paddingBottom: "14px",
         justifyContent: "center",
         alignItems: "center",
         gap: "8px",
@@ -19,7 +18,14 @@ const ButtonV2 = ({ icon, label, href, style }: Props) => {
         ...style,
       }}
     >
-      <a href={href}>
+      <a
+        href={href}
+        style={{
+          marginTop: "14px",
+          marginBottom: "14px",
+          width: "100%",
+        }}
+      >
         <div>{icon}</div>
         <div>{label}</div>
       </a>
